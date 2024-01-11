ce1433c1e617222fcd931fd2ff0858d986e39010
aln1234 <albinlamichhane9@gmail.com>
2024-01-11 09:19:17 +0545

    changes

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import express from 'express';
-
-
-
 const app = express();
 
-app.listen(5001,() => console.log('Api is running on port 5001'))
+app.listen(5001, () => console.log('Api is running on port 5001'));
 
-
-app.get("/",(res,req) => res.json("My api is running") )
+app.get('/', (req, res) => res.status(200).json('My api is running'));
